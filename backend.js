9d72d1df15257afdfc16e13afc89a842437a3bbb
Alex Keener <alexkeener318@gmail.com>
2023-11-21 22:44:09 -0600

    Adds endpoint to edit/update csv

--- backend.js
+++ backend.js
@@ -31,6 +31,17 @@ app.post('/write-file/:filename',express.text(), async (req, res) => {
   }
 });
 
+app.post('/edit-file/:filename', express.text(), async (req, res) => {
+  const { filename } = req.params;
+  const content = req.body;
+  try {
+    await editFile(filename, content);
+    res.send(JSON.stringify('File Updated'));
+  } catch (error) {
+    res.status(500).send('Error writing to file');
+  }
+});
+
 async function readFromFile(filename) {
   const filePath = `./${filename}`;
   const content = await fs.readFile(filePath, 'utf-8');
@@ -40,6 +51,62 @@ async function readFromFile(filename) {
 async function writeToFile(filename, content) {
   const filePath = `./${filename}`;
   await fs.appendFile(filePath, content, 'utf-8');
+}
+
+async function editFile(filename, content) {
+  const filePath = `./${filename}`;
+  try {
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+
+    const rows = fileContent.split('\n');
+    const values = content.split(','); 
+    const songArtist = values.slice(0, 2).join(','); 
+
+    let rowIndex = -1;
+    rows.some((row, index) => {
+      if (row.startsWith(songArtist)) {
+        rowIndex = index;
+        return true;
+      }
+    });
+
+    if (rowIndex !== -1) {
+      rows[rowIndex] = replaceValuesInRow(rows[rowIndex], values);
+
+      const updatedContent = rows.join('\n');
+
+      await fs.writeFile(filePath, updatedContent, 'utf-8');
+      
+      console.log('File updated successfully!');
+    } else {
+      console.log('Row not found.');
+    }
+  } catch (error) {
+    console.error('Error editing file:', error);
+  }
+}
+
+function replaceValuesInRow(rowString, newValues) {
+  // Splits current row into it's individual pieces
+  const values = rowString.split(/,\s*(?![^\[]*])/g);
+  
+  for (let i = 2; i < values.length; i++) {
+    // Gets rid of brackets for scores
+    values[i] = values[i].substring(0, values[i].length - 1)
+    // Adds user score to score list
+    values[i] += ","+ newValues[i].replace(/[\[\]]/g, '');
+
+    // Fixes error where last score list has an \n appended
+    values[i] = values[i].split('\n');
+    if (i === values.length - 1){
+      values[i] = values[i][0]
+    }
+    // Adds ending bracket
+    values[i] += ']';
+  }
+
+  const updatedRow = values.join(',');
+  return updatedRow;
 }
 
 app.listen(port, () => {
